baff05c78c80158122043fb19131a6c9b013b928
sandeepkasaragod <sandeep.kolya@gmail.com>
2024-09-11 20:22:28 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -102,12 +102,14 @@ update_gluetools_config() {
     config_file="gluetools-config.xml"
     if [[ -f "$config_file" ]]; then
         echo "Updating $config_file for $tool_name"
+				echo "Updating samtools temp directory in $config_file"
+    		sed -i.bak "s|<value>.*samfiles</value>|<value>$current_working_dir/samfiles</value>|g" $config_file
         
         case "$tool_name" in
             "blastn")
-                sed -i.bak "s|<value>.*blastn</value>|<value>$tool_path</value>|g" $config_file
-                sed -i.bak "s|<name>gluetools.core.programs.blast.temp.dir</name>.*<value>.*</value>|<name>gluetools.core.programs.blast.temp.dir</name><value>$current_working_dir/blastfiles</value>|g" $config_file
-                sed -i.bak "s|<name>gluetools.core.programs.blast.db.dir</name>.*<value>.*</value>|<name>gluetools.core.programs.blast.db.dir</name><value>$current_working_dir/blastdbs</value>|g" $config_file
+                sed -i.bak "s|<value>.blastn</value>|<value>$tool_path</value>|g" $config_file
+                sed -i.bak "s|<value>.*/blastfiles</value>|<value>$current_working_dir/blastfiles</value>|g" $config_file
+                sed -i.bak "s|<value>.*/blastdbs</value>|<value>$current_working_dir/blastdbs</value>|g" $config_file
                 ;;
             "tblastn")
                 sed -i.bak "s|<value>.*tblastn</value>|<value>$tool_path</value>|g" $config_file
@@ -117,15 +119,15 @@ update_gluetools_config() {
                 ;;
             "mafft")
                 sed -i.bak "s|<value>.*mafft</value>|<value>$tool_path</value>|g" $config_file
-                sed -i.bak "s|<name>gluetools.core.programs.mafft.temp.dir</name>.*<value>.*</value>|<name>gluetools.core.programs.mafft.temp.dir</name><value>$current_working_dir/mafftfiles</value>|g" $config_file
+                sed -i.bak "s|<value>.*/mafftfiles</value>|<value>$current_working_dir/mafftfiles</value>|g" $config_file
                 ;;
             "raxmlHPC")
                 sed -i.bak "s|<value>.*raxmlHPC</value>|<value>$tool_path</value>|g" $config_file
-                sed -i.bak "s|<name>gluetools.core.programs.raxml.temp.dir</name>.*<value>.*</value>|<name>gluetools.core.programs.raxml.temp.dir</name><value>$current_working_dir/raxmlfiles</value>|g" $config_file
+                sed -i.bak "s|<value>.*/raxmlfiles</value>|<value>$current_working_dir/raxmlfiles</value>|g" $config_file
                 ;;
             "table2asn")
                 sed -i.bak "s|<value>.*table2asn</value>|<value>$tool_path</value>|g" $config_file
-                sed -i.bak "s|<name>gluetools.core.programs.tbl2asn.temp.dir</name>.*<value>.*</value>|<name>gluetools.core.programs.tbl2asn.temp.dir</name><value>$current_working_dir/tbl2asnfiles</value>|g" $config_file
+                sed -i.bak "s|<value>.*/tbl2asn</value>|<value>$current_working_dir/tbl2asnfiles</value>|g" $config_file
                 ;;
             *)
                 echo "Unknown tool: $tool_name"
@@ -165,11 +167,11 @@ create_gluetools_config() {
 		</property>
 		<property>
 			<name>gluetools.core.programs.blast.temp.dir</name>
-			<value>$current_working_dir/blastfiles</value>
+			<value>$current_working_dir/tmp/blastfiles</value>
 		</property>
 		<property>
 			<name>gluetools.core.programs.blast.db.dir</name>
-			<value>$current_working_dir/blastdbs</value>
+			<value>$current_working_dir/tmp/blastdbs</value>
 		</property>
 		<!-- MAFFT specific config -->
 		<property>
@@ -178,7 +180,7 @@ create_gluetools_config() {
 		</property>
 		<property>
 			<name>gluetools.core.programs.mafft.temp.dir</name>
-			<value>$current_working_dir/mafftfiles</value>
+			<value>$current_working_dir/tmp/mafftfiles</value>
 		</property>
 		<!-- RAxML-specific config -->
 		<property>
@@ -187,7 +189,7 @@ create_gluetools_config() {
 		</property>
 		<property>
 			<name>gluetools.core.programs.raxml.temp.dir</name>
-			<value>$current_working_dir/raxmlfiles</value>
+			<value>$current_working_dir/tmp/raxmlfiles</value>
 		</property>
 		<!-- tbl2asn-specific config -->
 		<property>
@@ -196,7 +198,7 @@ create_gluetools_config() {
 		</property>
 		<property>
 			<name>gluetools.core.programs.tbl2asn.temp.dir</name>
-			<value>$current_working_dir/tbl2asnfiles</value>
+			<value>$current_working_dir/tmp/tbl2asnfiles</value>
 		</property>
 	</properties>
 </gluetools>
@@ -206,11 +208,11 @@ EOL
 if [[ "$conda_installed" == "false" ]]; then
     if [[ "$os_type" == "Darwin" && "$architecture" == "arm64" ]]; then
         echo "macOS on ARM architecture detected. Downloading Miniforge for ARM64..."
-        conda_install "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh" "$HOME/miniforge3" "$HOME/miniforge/etc/profile.d/conda.sh"
+        conda_install "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh" "$HOME/miniforge3" "$HOME/miniforge3/etc/profile.d/conda.sh"
 
     elif [[ "$os_type" == "Darwin" && "$architecture" == "x86_64" ]]; then
         echo "macOS on x86_64 architecture detected. Downloading Miniforge for x86_64..."
-        conda_install "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh" "$HOME/miniforge3" "$HOME/miniforge/etc/profile.d/conda.sh"
+        conda_install "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-x86_64.sh" "$HOME/miniforge3" "$HOME/miniforge3/etc/profile.d/conda.sh"
 
     elif [[ "$os_type" == "Linux" && "$architecture" == "x86_64" ]]; then
         echo "Linux on x86_64 architecture detected. Downloading Miniforge for x86_64..."
@@ -226,7 +228,8 @@ if [[ "$conda_installed" == "false" ]]; then
     setup_conda_env
 else
     echo "Skipping installation as Conda is already installed."
+		setup_conda_env
+    activate_gluetools_and_get_paths
 fi
 
-activate_gluetools_and_get_paths
-
+#activate_gluetools_and_get_paths
